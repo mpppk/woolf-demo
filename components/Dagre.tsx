2ec7b6f1876c201b3d534a44582d4db8b3cebbf8
mpppk <niboshiporipori@gmail.com>
2019-01-10 14:30:36 +0900

    Remove Page componetn

--- components/Dagre.tsx
+++ components/Dagre.tsx
@@ -1,54 +1,46 @@
 import * as d3 from 'd3';
 import * as dagreD3 from 'dagre-d3';
-import { Lamool } from 'lamool/src/lamool';
+import { Label } from 'dagre-d3';
 import React from 'react';
-import isEqual from 'react-fast-compare';
-import { Woolf } from 'woolf';
+// import isEqual from 'react-fast-compare';
 
-interface IDagreProps {
-  nodes: {};
-  edges: Array<[string, string, {}]>;
-  interactive: boolean;
-  fit: boolean;
-  height: string;
-  width: string;
-  shapeRenderers?: () => void;
-  onNodeClick?: (id: any) => void; // FIXME
-  zoom: number;
+export interface INode {
+  name: string;
+  label: Label;
 }
 
-class DagreD3 extends React.Component<IDagreProps> {
+export interface IEdge {
+  name: string;
+  targetId: string;
+  value: Label;
+}
+
+interface IDagreProps {
+  nodes: INode[];
+  edges: IEdge[];
+  zoom: number;
+  onComponentDidMount: () => void;
+}
+
+class Dagre extends React.Component<IDagreProps> {
   // tslint:disable-next-line member-access
   static defaultProps = {
-    fit: true,
-    height: '1',
-    interactive: false,
-    // width and height are defaulted to 1 due to a FireFox bug(?) If set to 0, it complains.
-    width: '1',
-    zoom: '2',
+    zoom: '2'
   };
-
-  private nodeTree: any;
-  private nodeTreeGroup: any;
-  private woolf: Woolf;
-
-  constructor(props) {
-    super(props);
-    this.woolf = new Woolf(new Lamool());
-    this.woolf.newJob();
-  }
+  private node: any;
 
   // tslint:disable-next-line member-access
-  shouldComponentUpdate(nextProps, _nextState) {
-    return (
-      !isEqual(this.props.nodes, nextProps.nodes) ||
-      !isEqual(this.props.edges, nextProps.edges) ||
-      !isEqual(this.props.zoom, nextProps.zoom)
-    );
-  }
+  // shouldComponentUpdate(nextProps, _nextState) {
+  //   return (
+  //     !isEqual(this.props.nodes, nextProps.nodes) ||
+  //     !isEqual(this.props.edges, nextProps.edges) ||
+  //     !isEqual(this.props.zoom, nextProps.zoom)
+  //   );
+  // }
 
   // tslint:disable-next-line member-access
   componentDidMount() {
+    this.props.onComponentDidMount();
     this.renderDag();
   }
 
@@ -58,81 +50,53 @@ class DagreD3 extends React.Component<IDagreProps> {
   }
 
   // tslint:disable-next-line member-access
-  render() {
-    return (
-      <svg
-        className="dagre-d3"
-        ref={r => {
-          this.nodeTree = r;
-        }}
-        width={this.props.height}
-        height={this.props.width}
-      >
-        <g
-          ref={r => {
-            this.nodeTreeGroup = r;
-          }}
-        />
-      </svg>
-    );
-  }
-  private renderDag() {
-    const g = new dagreD3.graphlib.Graph().setGraph({});
+  renderDag() {
+    // Create the input graph
+    const g = new dagreD3.graphlib.Graph()
+      .setGraph({})
+      .setDefaultEdgeLabel(() => {
+        return {};
+      });
 
-    for (const [id, node] of Object.entries(this.props.nodes)) {
-      g.setNode(id, node);
-    }
+    this.props.nodes.forEach((node) => { g.setNode(node.name, node.label); });
 
-    for (const edge of this.props.edges) {
-      g.setEdge(edge[0], edge[1], edge[2]); // from, to, props
-    }
-
-    // Set up an SVG group so that we can translate the final graph.
-    const svg = d3.select(this.nodeTree);
-    const inner = d3.select(this.nodeTreeGroup);
-
-    // set up zoom support
-    if (this.props.interactive) {
-      const zoom = d3
-        .zoom()
-        .on('zoom', () => inner.attr('transform', d3.event.transform));
-      svg.call(zoom);
-    }
+    g.nodes().forEach(v => {
+      const node = g.node(v);
+      // Round the corners of the nodes
+      node.rx = node.ry = 5;
+    });
+    this.props.edges.forEach((edge) => g.setEdge(edge.name, edge.targetId, edge.value));
 
     // Create the renderer
     const render = new dagreD3.render();
 
-    // set up custom shape renderers
-    if (this.props.shapeRenderers) {
-      for (const [shape, renderer] of Object.entries(
-        this.props.shapeRenderers
-      )) {
-        render.shapes()[shape] = renderer;
-      }
-    }
+    // Set up an SVG group so that we can translate the final graph.
+    const svg = d3
+      .select(this.node)
+      .attr('width', 600)
+      .attr('height', 300);
+    const svgGroup = svg.append('g');
+    const helloGroup = svg.append('hello');
+    helloGroup.text('hello!');
 
     // Run the renderer. This is what draws the final graph.
-    // @ts-ignore FIXME
-    render(inner, g);
+    const selector = svg.select('g');
+    render(selector as any, g);
+    // Center the graph
+    const width = parseInt(svg.attr('width'), 10);
+    const xCenterOffset = (width - g.graph().width) / 2;
+    svgGroup.attr('transform', 'translate(' + xCenterOffset + ', 20)');
+    svg.attr('height', g.graph().height + 40);
+    this.node = svg;
+  }
+  // private renderDag() {
+  //   const g = new dagreD3.graphlib.Graph().setGraph({});
+  // }
 
-    // TODO add padding?
-    if (this.props.fit) {
-      const { height: gHeight, width: gWidth } = g.graph();
-      const { height, width } = this.nodeTree.getBBox();
-      const transX = width - gWidth;
-      const transY = height - gHeight;
-      svg.attr('height', height);
-      svg.attr('width', width);
-      // @ts-ignore FIXME
-      inner.attr('transform', d3.zoomIdentity.translate(transX, transY));
-    }
-
-    if (this.props.onNodeClick) {
-      svg
-        .selectAll('.dagre-d3 .node')
-        .on('click', id => this.props.onNodeClick(id));
-    }
+  // tslint:disable-next-line member-access
+  render() {
+    return <svg ref={node => (this.node = node)}/>;
   }
 }
 
-export default DagreD3;
+export default Dagre;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,65 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import Typography from '@material-ui/core/Typography/Typography';
 import { counterActionCreators } from '../actions';
-import Page from '../components/Page';
+import AppBar from '../components/AppBar';
+import Dagre, { IEdge, INode } from '../components/Dagre';
+import LineChart from '../components/LineChart';
+
+const data = [
+  [
+    { x: 0, y: 6 },
+    { x: 1, y: 9 },
+    { x: 2, y: 6 },
+    { x: 3, y: 5 },
+    { x: 4, y: 2 },
+    { x: 6, y: 4 },
+    { x: 7, y: 2 },
+    { x: 8, y: 5 },
+    { x: 9, y: 2 }
+  ]
+];
+
+const styles = {
+  edge: {
+    arrowheadStyle: 'fill: #000',
+    style:
+      'fill: transparent; stroke: #000; stroke-width: 2px; stroke-dasharray: 5, 5;'
+  },
+  node: 'fill: #fff; stroke: #333; stroke-width: 1.5px;'
+};
+
+const nodes: INode[] = [
+  {
+    label: {
+      class: 'type-TOP',
+      label: 'TOP',
+      style: styles.node
+    },
+    name: '0'
+  },
+  {
+    label: {
+      class: 'type-S',
+      label: 'S',
+      style: styles.node
+    },
+    name: '1'
+  }
+];
+
+const edges: IEdge[] = [
+  {
+    name: '0',
+    targetId: '1',
+    value: { ...styles.edge }
+  }
+];
+
+const onDagreDidMount = () => {
+  console.log('dagre did mount'); // tslint:disable-line
+};
 
 class Index extends React.Component {
   // tslint:disable-next-line member-access
@@ -14,7 +71,21 @@ class Index extends React.Component {
 
   // tslint:disable-next-line member-access
   render() {
-    return <Page title="Index Page" />;
+    return (
+      <div>
+        <AppBar />
+        <Typography variant="h2" gutterBottom={true}>
+          Index Page
+        </Typography>
+        <LineChart data={data} />
+        <Dagre
+          nodes={nodes}
+          edges={edges}
+          onComponentDidMount={onDagreDidMount}
+        />
+      </div>
+    );
+    // return <Page title="Index Page" />;
   }
 }
 

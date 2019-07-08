c6997f5e3f2237def77ff06241c0c3a0cec7ad98
mpppk <niboshiporipori@gmail.com>
2019-07-08 13:41:58 +0900

    Add examples/hello_wolrd page and update side list

--- components/drawer/SideList.tsx
+++ components/drawer/SideList.tsx
@@ -1,44 +1,50 @@
-import { withStyles } from '@material-ui/core';
 import List from '@material-ui/core/List/List';
 import ListItem from '@material-ui/core/ListItem/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText/ListItemText';
-import HomeIcon from '@material-ui/icons/Home';
-import InfoIcon from '@material-ui/icons/Info';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Link from 'next/link';
 import * as React from 'react';
 
-const styles = {
-  list: {
-    width: 250
-  }
-};
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    nested: {
+      paddingLeft: theme.spacing(4)
+    },
+    root: {
+      backgroundColor: theme.palette.background.paper,
+      maxWidth: 360,
+      width: '100%'
+    }
+  })
+);
 
 // tslint:disable-next-line variable-name
-const SideList: React.FunctionComponent<{ classes }> = props => {
-  const { classes } = props;
+const SideList: React.FunctionComponent = () => {
+  const classes = useStyles(undefined);
   return (
-    <div className={classes.list}>
-      <List>
-        <Link href={'/'}>
-          <ListItem button={true} key={'Home'}>
-            <ListItemIcon>
-              <HomeIcon />
-            </ListItemIcon>
-            <ListItemText primary={'Home'} />
-          </ListItem>
-        </Link>
-        <Link href={'/about'}>
-          <ListItem button={true} key={'About'}>
-            <ListItemIcon>
-              <InfoIcon />
-            </ListItemIcon>
-            <ListItemText primary={'About'} />
+    <List className={classes.root}>
+      <Link href={'/'}>
+        <ListItem button={true} key={'Home'}>
+          <ListItemText primary={'Home'} />
+        </ListItem>
+      </Link>
+      <Link href={'/about'}>
+        <ListItem button={true} key={'About'}>
+          <ListItemText primary={'About'} />
+        </ListItem>
+      </Link>
+      <ListItem button={true} key={'Examples'}>
+        <ListItemText primary={'Examples'} />
+      </ListItem>
+      <List component="div" disablePadding={true}>
+        <Link href={'/examples/hello_world'}>
+          <ListItem button={true} className={classes.nested}>
+            <ListItemText primary="Hello World" />
           </ListItem>
         </Link>
       </List>
-    </div>
+    </List>
   );
 };
 
-export default withStyles(styles)(SideList);
+export default SideList;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,179 +1,20 @@
-import { Button } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { WoolfView } from 'react-woolf';
-import { bindActionCreators, Dispatch } from 'redux';
-import { JobFuncStat } from 'woolf/src/job';
-import { IWoolfData } from 'woolf/src/models';
-import { IJobStat } from 'woolf/src/scheduler/scheduler';
-import { IUpdateCurrentStatPayload, woolfActionCreators } from '../actions';
-import {
-  SampleSelectorActionCreators,
-  sampleSelectorActionCreators
-} from '../actions/sampleSelector';
+import Typography from '@material-ui/core/Typography/Typography';
 import AppBar from '../components/AppBar';
-import SampleSelector from '../components/SampleSelector';
-import StatTab from '../components/StatTab';
-import { State } from '../reducer';
-import { SampleName } from '../services/samples/Samples';
-
-type IndexProps = {
-  availableSampleNames: SampleName[];
-  stats: IJobStat[];
-  currentStat: IUpdateCurrentStatPayload;
-  woolfResults: IWoolfData[];
-  runPayload: IWoolfData;
-  sampleName: SampleName;
-} & ReturnType<typeof mapDispatchToProps> &
-  SampleSelectorActionCreators;
-
-interface IndexState {
-  tabValue: 0;
-}
-
-class Index extends React.Component<IndexProps, IndexState> {
-  // tslint:disable-next-line member-access
-  static async getInitialProps(props) {
-    const { isServer } = props.ctx;
-    return { isServer };
-  }
-
-  constructor(props) {
-    super(props);
-    this.handleClickRunButton = this.handleClickRunButton.bind(this);
-    this.handleClickFuncNode = this.handleClickFuncNode.bind(this);
-    this.handleClickJobNode = this.handleClickJobNode.bind(this);
-    this.handleClickStatTab = this.handleClickStatTab.bind(this);
-    this.handleClickInputNode = this.handleClickInputNode.bind(this);
-    this.handleClickOutputNode = this.handleClickOutputNode.bind(this);
-    this.handleSampleSelectorChange = this.handleSampleSelectorChange.bind(
-      this
-    );
-    this.state = { tabValue: 0 };
-  }
-
-  // tslint:disable-next-line member-access
-  componentDidMount(): void {
-    this.props.requestToAssemble({ sampleName: this.props.sampleName });
-  }
-
-  // tslint:disable-next-line member-access
-  handleClickFuncNode(jobStat: IJobStat, funcStat: JobFuncStat) {
-    this.props.updateCurrentStat({ jobStat, funcStat });
-  }
-
-  // tslint:disable-next-line member-access
-  handleClickJobNode(jobStat: IJobStat) {
-    this.props.updateCurrentStat({ jobStat });
-  }
-
-  // tslint:disable-next-line member-access
-  handleSampleSelectorChange(selectedSampleName: SampleName) {
-    this.props.change({ selectedSampleName });
-  }
-
-  // tslint:disable-next-line member-access
-  handleClickInputNode() {
-    // tslint:disable-next-line no-console
-    console.log('handleClickInputNode'); // FIXME
-  }
-
-  // tslint:disable-next-line member-access
-  handleClickOutputNode() {
-    // tslint:disable-next-line no-console
-    console.log('handleClickOutputNode'); // FIXME
-  }
-
-  // tslint:disable-next-line member-access
-  render() {
-    const funcStat = this.props.currentStat.funcStat;
-    const code = funcStat && funcStat.Code ? funcStat.Code : '<empty>';
-
-    // @ts-ignore
-    return (
-      <div>
-        <AppBar />
-        <Grid container={true} spacing={2}>
-          <Grid item={true} xs={12}>
-            <SampleSelector
-              currentSampleName={this.props.sampleName}
-              sampleNames={this.props.availableSampleNames}
-              onChange={this.handleSampleSelectorChange}
-            />
-          </Grid>
-          <Grid item={true} xs={8}>
-            <Paper>
-              {this.props.stats.length > 0 && (
-                <WoolfView
-                  width={800}
-                  height={500}
-                  stats={this.props.stats}
-                  onClickFuncNode={this.handleClickFuncNode}
-                  onClickJobNode={this.handleClickJobNode}
-                  onClickInputNode={this.handleClickInputNode}
-                  onClickOutputNode={this.handleClickOutputNode}
-                  showInput={true}
-                  inputNodeLabel={JSON.stringify(this.props.runPayload)}
-                  outputNodeLabel={JSON.stringify(this.props.woolfResults)}
-                  showOutput={true}
-                />
-              )}
-            </Paper>
-          </Grid>
-          <Grid item={true} xs={4}>
-            <Paper>
-              <StatTab
-                code={code.toString()}
-                funcStat={funcStat}
-                jobStat={this.props.currentStat.jobStat}
-                onClickTab={this.handleClickStatTab}
-                tabValue={this.state.tabValue}
-              />
-            </Paper>
-          </Grid>
-        </Grid>
-        <Button variant="contained" onClick={this.handleClickRunButton}>
-          Run
-        </Button>
-      </div>
-    );
-  }
-
-  private async handleClickRunButton() {
-    this.props.requestToRun({});
-  }
-
-  private handleClickStatTab(tabValue: any) {
-    this.setState({ ...this.state, tabValue });
-  }
-}
-
-const mapStateToProps = (state: State): Partial<IndexProps> => {
-  return {
-    availableSampleNames: state.availableSampleNames,
-    currentStat: state.currentStat,
-    runPayload: state.runPayload,
-    sampleName: state.sampleName,
-    stats: state.stats,
-    woolfResults: state.woolfResults
-  };
-};
-
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return {
-    ...bindActionCreators(
-      {
-        ...woolfActionCreators,
-        ...sampleSelectorActionCreators
-      },
-      dispatch
-    ) // FIXME
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Index);
+export default () => (
+  <div>
+    <AppBar />
+    <Typography variant="h2" gutterBottom={true}>
+      Woolf
+    </Typography>
+    Woolf is the <b>Universal</b> Workflow Engine like AWS Step Functions.
+    <br />
+    Universal means that Woolf can work on
+    <br />
+    * browser
+    <br />
+    * Nodejs
+    <br />
+    * AWS Lambda (of course!)
+    <br />
+  </div>
+);

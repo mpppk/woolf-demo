f13e8ad66d3998ce3346efedf05b850a5af8140d
mpppk <niboshiporipori@gmail.com>
2019-07-08 14:26:24 +0900

    Use react-redux hooks api in hello world page

--- pages/examples/hello_world.tsx
+++ pages/examples/hello_world.tsx
@@ -2,178 +2,96 @@ import { Button } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import * as React from 'react';
-import { connect } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { WoolfView } from 'react-woolf';
-import { bindActionCreators, Dispatch } from 'redux';
+import { IJobStat } from 'woolf';
 import { JobFuncStat } from 'woolf/src/job';
-import { IWoolfData } from 'woolf/src/models';
-import { IJobStat } from 'woolf/src/scheduler/scheduler';
-import { IUpdateCurrentStatPayload, woolfActionCreators } from '../../actions';
-import {
-  SampleSelectorActionCreators,
-  sampleSelectorActionCreators
-} from '../../actions/sampleSelector';
+import { woolfActionCreators } from '../../actions';
+import { sampleSelectorActionCreators } from '../../actions/sampleSelector';
 import AppBar from '../../components/AppBar';
 import SampleSelector from '../../components/SampleSelector';
 import StatTab from '../../components/StatTab';
 import { State } from '../../reducer';
 import { SampleName } from '../../services/samples/Samples';
 
-type IndexProps = {
-  availableSampleNames: SampleName[];
-  stats: IJobStat[];
-  currentStat: IUpdateCurrentStatPayload;
-  woolfResults: IWoolfData[];
-  runPayload: IWoolfData;
-  sampleName: SampleName;
-} & ReturnType<typeof mapDispatchToProps> &
-  SampleSelectorActionCreators;
+// tslint:disable-next-line variable-name
+const HelloWorld: React.FunctionComponent = () => {
+  const state = useSelector((s: State) => s);
+  const [tabValue, setTabValue] = useState(0);
+  const dispatch = useDispatch();
 
-interface IndexState {
-  tabValue: 0;
-}
+  useEffect(() => {
+    if (state.stats.length === 0) {
+      dispatch(
+        woolfActionCreators.requestToAssemble({ sampleName: state.sampleName })
+      );
+    }
+  });
 
-class HelloWorld extends React.Component<IndexProps, IndexState> {
-  // tslint:disable-next-line member-access
-  static async getInitialProps(props) {
-    const { isServer } = props.ctx;
-    return { isServer };
-  }
+  const handleClickFuncNode = (jobStat: IJobStat, fs: JobFuncStat) => {
+    dispatch(woolfActionCreators.updateCurrentStat({ jobStat, funcStat: fs }));
+  };
 
-  constructor(props) {
-    super(props);
-    this.handleClickRunButton = this.handleClickRunButton.bind(this);
-    this.handleClickFuncNode = this.handleClickFuncNode.bind(this);
-    this.handleClickJobNode = this.handleClickJobNode.bind(this);
-    this.handleClickStatTab = this.handleClickStatTab.bind(this);
-    this.handleClickInputNode = this.handleClickInputNode.bind(this);
-    this.handleClickOutputNode = this.handleClickOutputNode.bind(this);
-    this.handleSampleSelectorChange = this.handleSampleSelectorChange.bind(
-      this
-    );
-    this.state = { tabValue: 0 };
-  }
+  const handleClickJobNode = (jobStat: IJobStat) => {
+    dispatch(woolfActionCreators.updateCurrentStat({ jobStat }));
+  };
 
-  // tslint:disable-next-line member-access
-  componentDidMount(): void {
-    this.props.requestToAssemble({ sampleName: this.props.sampleName });
-  }
+  const handleSampleSelectorChange = (selectedSampleName: SampleName) => {
+    dispatch(sampleSelectorActionCreators.change({ selectedSampleName }));
+  };
 
-  // tslint:disable-next-line member-access
-  handleClickFuncNode(jobStat: IJobStat, funcStat: JobFuncStat) {
-    this.props.updateCurrentStat({ jobStat, funcStat });
-  }
+  const handleClickRunButton = () => {
+    dispatch(woolfActionCreators.requestToRun({})); // FIXME
+  };
 
-  // tslint:disable-next-line member-access
-  handleClickJobNode(jobStat: IJobStat) {
-    this.props.updateCurrentStat({ jobStat });
-  }
-
-  // tslint:disable-next-line member-access
-  handleSampleSelectorChange(selectedSampleName: SampleName) {
-    this.props.change({ selectedSampleName });
-  }
-
-  // tslint:disable-next-line member-access
-  handleClickInputNode() {
-    // tslint:disable-next-line no-console
-    console.log('handleClickInputNode'); // FIXME
-  }
-
-  // tslint:disable-next-line member-access
-  handleClickOutputNode() {
-    // tslint:disable-next-line no-console
-    console.log('handleClickOutputNode'); // FIXME
-  }
-
-  // tslint:disable-next-line member-access
-  render() {
-    const funcStat = this.props.currentStat.funcStat;
-    const code = funcStat && funcStat.Code ? funcStat.Code : '<empty>';
-
-    // @ts-ignore
-    return (
-      <div>
-        <AppBar />
-        <Grid container={true} spacing={2}>
-          <Grid item={true} xs={12}>
-            <SampleSelector
-              currentSampleName={this.props.sampleName}
-              sampleNames={this.props.availableSampleNames}
-              onChange={this.handleSampleSelectorChange}
-            />
-          </Grid>
-          <Grid item={true} xs={8}>
-            <Paper>
-              {this.props.stats.length > 0 && (
-                <WoolfView
-                  width={800}
-                  height={500}
-                  stats={this.props.stats}
-                  onClickFuncNode={this.handleClickFuncNode}
-                  onClickJobNode={this.handleClickJobNode}
-                  onClickInputNode={this.handleClickInputNode}
-                  onClickOutputNode={this.handleClickOutputNode}
-                  showInput={true}
-                  inputNodeLabel={JSON.stringify(this.props.runPayload)}
-                  outputNodeLabel={JSON.stringify(this.props.woolfResults)}
-                  showOutput={true}
-                />
-              )}
-            </Paper>
-          </Grid>
-          <Grid item={true} xs={4}>
-            <Paper>
-              <StatTab
-                code={code.toString()}
-                funcStat={funcStat}
-                jobStat={this.props.currentStat.jobStat}
-                onClickTab={this.handleClickStatTab}
-                tabValue={this.state.tabValue}
-              />
-            </Paper>
-          </Grid>
+  const funcStat = state.currentStat.funcStat;
+  const code = funcStat && funcStat.Code ? funcStat.Code : '<empty>';
+  return (
+    <div>
+      <AppBar />
+      <Grid container={true} spacing={2}>
+        <Grid item={true} xs={12}>
+          <SampleSelector
+            currentSampleName={state.sampleName}
+            sampleNames={state.availableSampleNames}
+            onChange={handleSampleSelectorChange}
+          />
         </Grid>
-        <Button variant="contained" onClick={this.handleClickRunButton}>
-          Run
-        </Button>
-      </div>
-    );
-  }
-
-  private async handleClickRunButton() {
-    this.props.requestToRun({});
-  }
-
-  private handleClickStatTab(tabValue: any) {
-    this.setState({ ...this.state, tabValue });
-  }
-}
-
-const mapStateToProps = (state: State): Partial<IndexProps> => {
-  return {
-    availableSampleNames: state.availableSampleNames,
-    currentStat: state.currentStat,
-    runPayload: state.runPayload,
-    sampleName: state.sampleName,
-    stats: state.stats,
-    woolfResults: state.woolfResults
-  };
+        <Grid item={true} xs={8}>
+          <Paper>
+            {state.stats.length > 0 && (
+              <WoolfView
+                width={800}
+                height={500}
+                stats={state.stats}
+                onClickFuncNode={handleClickFuncNode}
+                onClickJobNode={handleClickJobNode}
+                showInput={true}
+                inputNodeLabel={JSON.stringify(state.runPayload)}
+                outputNodeLabel={JSON.stringify(state.woolfResults)}
+                showOutput={true}
+              />
+            )}
+          </Paper>
+        </Grid>
+        <Grid item={true} xs={4}>
+          <Paper>
+            <StatTab
+              code={code.toString()}
+              funcStat={funcStat}
+              jobStat={state.currentStat.jobStat}
+              onClickTab={setTabValue}
+              tabValue={tabValue}
+            />
+          </Paper>
+        </Grid>
+      </Grid>
+      <Button variant="contained" onClick={handleClickRunButton}>
+        Run
+      </Button>
+    </div>
+  );
 };
 
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return {
-    ...bindActionCreators(
-      {
-        ...woolfActionCreators,
-        ...sampleSelectorActionCreators
-      },
-      dispatch
-    ) // FIXME
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(HelloWorld);
+export default HelloWorld;
